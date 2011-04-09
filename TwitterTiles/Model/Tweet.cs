f61c6a4490b388f6ed03499f31ab4e718743aa7e
Chris Koenig <chris.koenig@microsoft.com>
2011-04-09 00:52:18 -0500

    first commit

---
+++ TwitterTiles/Model/Tweet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TwitterTiles.Model
+{
+    public class Tweet
+    {
+        public string Avatar { get; set; }
+
+        public string Handle { get; set; }
+
+        public string Location { get; set; }
+
+        public string TweetText { get; set; }
+    }
+}
---
+++ TwitterTiles/ViewModel/MainViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+using TwitterTiles.Model;
+
+namespace TwitterTiles.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                Tweets.Add(new Tweet() { Avatar = "/Images/avatar.jpg", Handle = "Handle1", Location = "Frisco, TX", TweetText = "This is my latest tweet. There are many like them but this one is mine." });
+                Tweets.Add(new Tweet() { Avatar = "/Images/avatar.jpg", Handle = "Handle2", Location = "Frisco, TX", TweetText = "This is my latest tweet. There are many like them but this one is mine." });
+                Tweets.Add(new Tweet() { Avatar = "/Images/avatar.jpg", Handle = "Handle3", Location = "Frisco, TX", TweetText = "This is my latest tweet. There are many like them but this one is mine." });
+                Tweets.Add(new Tweet() { Avatar = "/Images/avatar.jpg", Handle = "Handle4", Location = "Frisco, TX", TweetText = "This is my latest tweet. There are many like them but this one is mine." });
+                Tweets.Add(new Tweet() { Avatar = "/Images/avatar.jpg", Handle = "Handle5", Location = "Frisco, TX", TweetText = "This is my latest tweet. There are many like them but this one is mine." });
+                Tweets.Add(new Tweet() { Avatar = "/Images/avatar.jpg", Handle = "Handle6", Location = "Frisco, TX", TweetText = "This is my latest tweet. There are many like them but this one is mine." });
+            }
+            else
+            {
+                // Code runs "for real"
+            }
+        }
+
+        #region Tweets property
+
+        private ObservableCollection<Tweet> _tweets = new ObservableCollection<Tweet>();
+
+        public ObservableCollection<Tweet> Tweets
+        {
+            get
+            {
+                return _tweets;
+            }
+
+            set
+            {
+                if (_tweets == value)
+                {
+                    return;
+                }
+
+                var oldValue = _tweets;
+                _tweets = value;
+                RaisePropertyChanged(() => this.Tweets);
+            }
+        }
+
+        #endregion Tweets property
+
+        #region SelectedTweet property
+
+        private Tweet _selectedTweet = null;
+
+        public Tweet SelectedTweet
+        {
+            get
+            {
+                return _selectedTweet;
+            }
+
+            set
+            {
+                if (_selectedTweet == value)
+                {
+                    return;
+                }
+
+                var oldValue = _selectedTweet;
+                _selectedTweet = value;
+                RaisePropertyChanged(() => this.SelectedTweet);
+            }
+        }
+
+        #endregion SelectedTweet property
+    }
+}
